1260d5c27f07548a04f27ecc10ca92ff170d8467
Julia Yadova <yadova.julia@yandex.ru>
2022-07-11 02:06:22 +0300

    Third commit before rew.Fix debug on+ allowed hosts+defconfing

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.154.194.146', 'web', ]
+ALLOWED_HOSTS = ['178.154.194.146', 'localhost', '127.0.0.1', ]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
